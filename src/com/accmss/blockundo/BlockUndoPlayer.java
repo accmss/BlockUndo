0d4aafd5c6e529c4fb6aabf204cc3648fdf399a1
accmss <sales@accmss.com>
2013-01-20 12:58:34 +1100

    .

--- src/com/accmss/blockundo/BlockUndoPlayer.java
+++ src/com/accmss/blockundo/BlockUndoPlayer.java
@@ -2,9 +2,12 @@ package com.accmss.blockundo;
 
 
 //IMPORTS - BUKKIT
+import org.bukkit.World;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 
@@ -35,5 +38,56 @@ public void onPlayerJoin(final PlayerJoinEvent event)
 
 }
 
+
+@EventHandler (priority = EventPriority.NORMAL)
+public void onPlayerInteract(final PlayerInteractEvent event) {
+	
+	World MyWorld = event.getPlayer().getWorld();
+	
+		if (BlockUndoLib.IsWorldProtected(MyWorld.getName().toString())) 
+		{
+
+			if (event.getAction() == Action.RIGHT_CLICK_BLOCK)
+			{
+
+
+				switch (event.getPlayer().getItemInHand().getType())
+				{
+				case GOLD_PICKAXE:
+
+					if (event.getPlayer().hasPermission("blockundo.player"))
+					{
+					BlockUndoLib.estimatedTime = System.nanoTime() - BlockUndoLib.startTime;
+						
+				    new Thread(new Runnable() { 
+				        public void run() { 
+				        	try {
+								Thread.sleep(2);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+							BlockUndoLib.Query(event.getPlayer(), event.getClickedBlock().getLocation());
+				        	} 
+				    }).start(); 
+				    
+
+
+					//NEW cool off (manipulates variable from 1 const thread)
+					BlockUndoLib.startTime = System.nanoTime();        
+					}
+				
+					
+
+					default:
+					break;
+					}
+			
+			}
+			
+
+		}
+			
+
+}
 
 }
